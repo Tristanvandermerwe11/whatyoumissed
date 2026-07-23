0d09fd2cc4306a2864e712e6ec58aecf1c9bae36
tristan <st10471259@myemeris.edu.za>
2026-07-23 15:26:27 +0200

    Add files via upload

---
+++ overloadingOperatiorExample/overloadingOperatiorExample/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace overloadingOperatiorExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector(6, 8);
+            Vector v2 = new Vector(8, 9);
+
+            // without operator overloading
+
+            Vector resault1 = v1.Add(v2);
+
+            Console.WriteLine($"resault without overload: ({resault1.X},{resault1.Y})");
+
+
+            // with operator overloading
+
+            Vector resault2 = v1 + v2;
+
+            Console.WriteLine($"resault with overload: ({resault2.X},{resault2.Y})");
+        }
+
+
+        
+    }
+}
---
+++ overloadingOperatiorExample/overloadingOperatiorExample/Vector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+
+namespace overloadingOperatiorExample
+{
+    class Vector
+    {
+        
+
+        public int Y { get; set; }
+        public int X { get; set; }
+
+        public Vector(int x, int y)
+        {
+            X = x;
+            Y = y;
+
+        }
+        // without operator overloading, we can make use of the following method to add 2 vectors
+
+        public Vector Add(Vector other)
+        {
+            return new Vector(X + other.X, Y + other.Y);
+        }
+
+
+        //overload the + to add 2 vectors
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
+        }
+
+
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y);
+        }
+    }
+}
