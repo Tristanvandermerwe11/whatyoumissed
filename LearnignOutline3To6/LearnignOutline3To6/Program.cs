24b35ebe38457157ec28dda24d67dde3997b5093
tristan <st10471259@myemeris.edu.za>
2026-07-28 13:16:14 +0200

    PROG

---
+++ LearnignOutline3To6/LearnignOutline3To6/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace LearnignOutline3To6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //without custom type
+            //string name = "alice";
+            //int mark = 36;
+
+            //with custom types
+            Student student = new Student("Max", 90);
+
+            //operator Overloading
+            student = student + 5;
+
+            Console.WriteLine("Student Info:");
+            Console.WriteLine("----------------");
+            Console.WriteLine("Name: " + student.Name);
+            Console.WriteLine("Marks: " + student.Marks);
+
+            //extension method
+            Console.WriteLine("grade: " + student.GetGrade());
+
+
+            //anonamous type
+            var summary = new
+            {
+                StudentName = student.Name,
+                FinalMarks = student.Marks,
+                Grade = student.GetGrade()
+            };
+
+            Console.WriteLine("Student Info:");
+            Console.WriteLine("----------------");
+            Console.WriteLine("Name: " + summary.StudentName);
+            Console.WriteLine("Marks: " + summary.FinalMarks);
+            Console.WriteLine("Marks: " + summary.Grade);
+
+
+
+
+        }
+    }
+}
---
+++ LearnignOutline3To6/LearnignOutline3To6/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnignOutline3To6
+{
+    class Student
+    {
+        //custom types
+        public string Name { get; set; }
+
+        public int Marks { get; set; }
+
+        public Student(string name, int marks)
+        {
+            Name = name;
+            Marks = marks;
+        }
+
+        //operator overloading
+
+        public static Student operator +(Student s, int bonusMarks)
+        {
+            return new Student(s.Name, s.Marks + bonusMarks);
+        }
+    }
+    //Extension method
+    static class StudentExtensions
+    {
+        public static string GetGrade(this Student stud)
+        {
+            if (stud.Marks >= 75)
+            {
+                return "Distinction";
+            }
+            else if (stud.Marks >= 50)
+            {
+                return "Pass";
+            }
+            else
+            {
+                return "Fail";
+            }
+        }
+    }
+}
