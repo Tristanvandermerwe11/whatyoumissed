fcfca6febdff6b3a78cc61167444a7cf798ff1fe
tristan <st10471259@myemeris.edu.za>
2026-07-30 13:18:30 +0200

    PROG

---
+++ linqExample/linqExample/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace linqExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
